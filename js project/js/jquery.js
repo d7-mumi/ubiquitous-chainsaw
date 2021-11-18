cf6280abf6ee90664ca1859fd617e865f7070a28
Herse <d7.ersa@gmail.com>
2021-11-18 17:52:56 +0100

    Section team members and carousel

--- js project/js/jquery.js
+++ js project/js/jquery.js
@@ -2,3 +2,27 @@ $(window).on('load', function(){
     $('#status').fadeOut();
     $('#preloader').delay(350).fadeOut('slow');
 });
+
+
+/**
+ * Team
+ */
+
+$(function(){
+    $('#team-member').owlCarousel({
+        loop:true,
+        margin: 10,
+        nav:true,
+        responsive:{
+            0:{
+                items:1
+            },
+            600:{
+                items:3
+            },
+            1000:{
+                items:5
+            }
+        }
+    })
+})
