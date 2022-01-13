15f955a689dfa163df06a76b2c6a13d8b75079dc
Ersa Doka <ersa.doka@innvis.io>
2022-01-13 21:48:25 +0100

    Promises

--- js/Asynchronous and AJAX and API call/script.js
+++ js/Asynchronous and AJAX and API call/script.js
@@ -60,43 +60,70 @@ const renderCountry = function(data, className = ''){
 }
 
 
-//AJAX call coutnry 1
-const getCountryAndNeighbour = function(country){
-    const request = new XMLHttpRequest();
-    request.open('GET', `https://restcountries.com/v3.1/name/${country}`);
-    request.send();
+// //AJAX call coutnry 1
+// const getCountryAndNeighbour = function(country){
+//     const request = new XMLHttpRequest();
+//     request.open('GET', `https://restcountries.com/v3.1/name/${country}`);
+//     request.send();
 
-    request.addEventListener('load', function(){
-        //console.log(this.responseText);
-        const [data]  = JSON.parse(this.responseText);
-        console.log(data);
+//     request.addEventListener('load', function(){
+//         //console.log(this.responseText);
+//         const [data]  = JSON.parse(this.responseText);
+//         console.log(data);
 
-        //Render country 1
-        renderCountry(data);
+//         //Render country 1
+//         renderCountry(data);
 
-        //get neighbour country 2
-        const [...neighbour] = data.borders;
+//         //get neighbour country 2
+//         const [...neighbour] = data.borders;
 
-        console.log(neighbour);
+//         console.log(neighbour);
 
-        if(!neighbour)  return;
+//         if(!neighbour)  return;
 
-            ///AJAX call country 2
-            for(let i = 0; i < neighbour.length; i++ ){
-                const request2 = new XMLHttpRequest();
-                request2.open('GET', `https://restcountries.com/v3.1/alpha/${neighbour[i]}`);
-                request2.send();
+//             ///AJAX call country 2
+//             for(let i = 0; i < neighbour.length; i++ ){
+//                 const request2 = new XMLHttpRequest();
+//                 request2.open('GET', `https://restcountries.com/v3.1/alpha/${neighbour[i]}`);
+//                 request2.send();
 
-                request2.addEventListener('load', function(){
-                    const [data2] = JSON.parse(this.responseText);
-                    console.log(data2);
+//                 request2.addEventListener('load', function(){
+//                     const [data2] = JSON.parse(this.responseText);
+//                     console.log(data2);
 
-                    renderCountry(data2, 'neighbour');
-                }); 
-            }
+//                     renderCountry(data2, 'neighbour');
+//                 }); 
+//             }
             
         
-    });
-}
+//     });
+// }
 
-getCountryAndNeighbour('serbia');
+// getCountryAndNeighbour('serbia');
+
+
+const getCountryData = function(country){
+    fetch(`https://restcountries.com/v3.1/name/${country}`)
+        .then(response => response.json())
+        .then(data => {
+            //console.log(data);
+            renderCountry(data[0]);
+            const neighbour = data[0].borders[0];
+            //console.log(neighbour);
+            if(!neighbour) return;
+
+            //country 2
+            return fetch(`https://restcountries.com/v3.1/alpha/${neighbour}`)
+        })
+        .then(response => {
+            console.log(response);
+            return response.json();
+        })
+        .then(data => {
+            console.log(data);
+            return renderCountry(data[0], 'neighbour');
+        });
+};
+
+getCountryData('albania')
+
